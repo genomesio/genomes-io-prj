f9dba8d05353d4bd0783de727de99416c471bc37
trvinh <tran@bio.uni-frankfurt.de>
2019-11-06 09:07:43 +0100

    moved run mode parameter to first arg for imputation_a2z

--- imputation/imputation_a2z.R
+++ imputation/imputation_a2z.R
@@ -1,48 +1,52 @@
-pkgs <- c("openxlsx", "jsonlite", "tools")
+pkgs <- c("openxlsx", "jsonlite")
 newPkgs <- pkgs[!(pkgs %in% installed.packages()[,"Package"])]
-if(length(newPkgs)) install.packages(newPkgs, repos = "http://cran.us.r-project.org")
+if(length(newPkgs)) install.packages(newPkgs)
 lapply(pkgs, library, character.only = TRUE)
+library(tools)
 
 #' Arguments
-#' 1: input ID
-#' 2: run dir (e.g. "imputation")
-#' 3: output dir
-#' 4: shapeit
-#' 5: plink
-#' 6: gtool
-#' 7: impute2
-#' 8: sample_ref
-#' 9: imputation reference data dir
-#' 10: impute-me dir
+#' 1: full or test only
+#' 2: input ID
+#' 3: run dir (e.g. "imputation")
+#' 4: output dir
+#' 5: shapeit
+#' 6: plink
+#' 7: gtool
+#' 8: impute2
+#' 9: sample_ref
+#' 10: imputation reference data dir
+#' 11: impute-me dir
 
 args = commandArgs(trailingOnly = TRUE)
 if (length(args) < 10) {
     msg <- paste(
         "You need to give all these args:",
-        "1: input ID",
-        "2: imputation dir",
-        "3: output dir",
-        "4: path to shapeit",
-        "5: path to plink",
-        "6: path to gtool",
-        "7: path to impute2",
-        "8: path to sample_ref",
-        "9: path to imputation reference data",
-		"10: path to the impute-me tool"
+		"1: run mode, either full or test",
+        "2: input ID",
+        "3: imputation dir",
+        "4: output dir",
+        "5: path to shapeit",
+        "6: path to plink",
+        "7: path to gtool",
+        "8: path to impute2",
+        "9: path to sample_ref",
+        "10: path to imputation reference data",
+		"11: path to the impute-me tool"
     )
     stop(paste0(msg, "\n"), call. = FALSE)
 }
 
-uniqueID <- args[1]
-runDir <- args[2]
-destinationDir <- args[3]
-shapeit <- args[4]
-plink <- args[5]
-gtool <- args[6]
-impute2 <- args[7]
-sample_ref <- args[8]
-imputeDataDir <- args[9]
-impute_me <- args[10]
+mode <- args[1]
+uniqueID <- args[2]
+runDir <- args[3]
+destinationDir <- args[4]
+shapeit <- args[5]
+plink <- args[6]
+gtool <- args[7]
+impute2 <- args[8]
+sample_ref <- args[9]
+imputeDataDir <- args[10]
+impute_me <- args[11]
 
 # check imputation dir
 if (!file.exists(paste0(runDir, "/", uniqueID, "_raw_data.txt"))) {
@@ -52,11 +56,14 @@ if (!file.exists(paste0(runDir, "/", uniqueID, "_raw_data.txt"))) {
 # MAIN
 source("imputation_fn.R")
 
-# step 1: run imputation
-run_imputation(runDir, shapeit, plink, impute2, imputeDataDir, sample_ref)
-# step 2: summarize imputation results
-summarize_imputation(runDir, uniqueID, destinationDir, gtool, plink)
-# step 3: get SNPs to analyze
-crawl_for_snps_to_analyze(uniqueID, impute_me, destinationDir)
+if (mode == "full") {
+    # step 1: run imputation
+    run_imputation(runDir, shapeit, plink, impute2, imputeDataDir, sample_ref)
+    # step 2: summarize imputation results
+    summarize_imputation(runDir, uniqueID, destinationDir, gtool, plink)
+    # step 3: get SNPs to analyze
+    crawl_for_snps_to_analyze(uniqueID, impute_me, destinationDir)
+}
+
 # step 4: export test results
 allResults <- run_export_script(uniqueID = uniqueID, modules = NULL, impute_me = impute_me, destinationDir = destinationDir, functionFile = tools::file_path_as_absolute("imputation_fn.R"), gtool = gtool)
